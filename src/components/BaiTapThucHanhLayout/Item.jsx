2532118cd6adb008d735536af92b64e0e18331a3
Tien Hoai <nguyentien1668@gmail.com>
2023-08-03 21:26:43 +0700

    upload img

--- src/components/BaiTapThucHanhLayout/Item.jsx
+++ src/components/BaiTapThucHanhLayout/Item.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-
+import image from "../../assets/img/anh.jpg"
 export default class Item extends Component {
   render() {
     return (
       <div>
         <div className="card bg-light border-0 h-100">
-          <div className=" d-inline-block" style={{ height: 160, backgroundColor: 'rgba(200,201,200,1)' }}></div>
+          <div>
+            <img className='w-100' src={image} alt="" />
+          </div>
           <div className="card-body text-center ">
+          {/* className=" d-inline-block" style={{ height: 160, backgroundColor: 'rgba(200,201,200,1)' }} */}
             <h4 className=" fw-bold">Cart title</h4>
             <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolorem repellendus at quam cumque fugiat animi, odit nisi.</p>
           </div>
